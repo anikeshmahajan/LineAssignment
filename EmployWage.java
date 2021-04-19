50150b82a2b6854853e2e5c0f2d675537b81b1d7
Anikesh <anikesh@0725@gmail.com>
2021-04-19 07:04:27 +0530

    Addition : Code added

---
+++ EmployWage.java
@@ -0,0 +1,16 @@
+
+public class EmployWage {
+public static void main(String[] args) {
+	
+	
+	
+	if(Math.floor(Math.random()*10)%2 == 1.0)
+	{
+		System.out.println("Employe is present");
+	}
+	else
+	{
+		System.out.println("Employ is absent");
+	}
+}
+}
